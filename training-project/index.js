2e62599aa26b94c1ce572d17f5dbcc25104d1ea6
Douglas Neres da S <douglasneres06@gmail.com>
2023-09-18 23:22:22 -0300

    Integration with Database

--- training-project/index.js
+++ training-project/index.js
@@ -1,8 +1,13 @@
 const { Person } = require("./person");
+const dotenv = require("dotenv");
+const connectToDatabase = require("./src/database/connect");
+
+dotenv.config();
+
+connectToDatabase();
 
 /*require("./modules/path")*/
 /* require("./modules/fs") */
 /* require("./modules/http") */
-require("./modules/express")
-
-const person = new Person('Douglas Neres');
+// require("./modules/express");
+// const person = new Person("Douglas Neres");
